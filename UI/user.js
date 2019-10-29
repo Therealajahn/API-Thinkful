16222473ea6fe9e92c5512c5bfed9e1b418ad071
A'Jahn Kimble <ajahnkimble@gmail.com>
2019-10-28 21:49:08 -0600

    begginning to generate elements in js

--- UI/user.js
+++ UI/user.js
@@ -1,12 +1,41 @@
-function getAmazonPage(){
+    //renders section element
+    //calls functions that render the guts
+function renderSection(){
+  console.log("render-section");   
+};
 
-let url = "https://www.amazon.com/Wander-Sneakers-Fashion-Lightweight-Athletic/dp/B07MXTMSXG?pf_rd_p=ca7593d6-e5ab-48e7-9133-65a8a08a432e&pd_rd_wg=z41I6&pf_rd_r=777YRRJ03YSFZ2Y8Z7Z6&ref_=pd_gw_srecs_l2ra&pd_rd_w=blMpS&pd_rd_r=194ea976-0a6c-4893-a72d-d242636c4c05"    
-    $(".link").on("click",()=>{
-    console.log("link clicked")
-    $.getJSON(url, result => {
-        console.log(result)
-    });
-})
-}
+//function renderCheck(){
+//    
+//};
+//
+//function addPriceToTotal(){
+//    
+//};
+//
+//function renderDeleteButton(){
+//    
+//};
+//
+//function renderForm(){
+//    
+//};
+//
+//function renderStars(){
+//    
+//};
+//
+//
+//function starHoverResponse(){
+//    
+//};
+//
+//function starClick(){
+//    
+//};
+//
+//function saveToSpreadSheet(){
+//    
+//};
 
-$(getAmazonPage);
+$(renderSection());
+

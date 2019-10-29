0772f1503f9af59061f3146d31d1406d600f3775
A'Jahn Kimble <ajahnkimble@gmail.com>
2019-10-28 22:32:28 -0600

    added store.js to store input and star values

---
+++ UI/state.js
@@ -0,0 +1,14 @@
+let state = {
+    section1:{
+        part1:{
+            name:"",
+            price:"",
+            link:"",
+            stars:""
+        }
+    }
+        
+    
+    
+}
+    
--- UI/user.js
+++ UI/user.js
@@ -1,28 +1,56 @@
     //renders section element
     //calls functions that render the guts
 function renderSection(){
-  console.log("render-section");   
+ let section = `<section class='section'>
+                ${renderPart()}
+                </section>`
+ $("body").html(section);
 };
 
-//function renderCheck(){
-//    
-//};
-//
+function renderPart(){
+    let part = `<section class='part'>
+                ${renderCheck()}
+                ${renderDeleteButton()}
+                ${renderForm()}
+                ${renderStars()}
+                </section>`
+    return part;
+}
+
+function renderCheck(){
+    let check = `<div class="check">
+                </div>`;
+    return check;
+};
+
 //function addPriceToTotal(){
 //    
 //};
-//
-//function renderDeleteButton(){
-//    
-//};
-//
-//function renderForm(){
-//    
-//};
-//
-//function renderStars(){
-//    
-//};
+
+function renderDeleteButton(){
+    let deleteButton = `<div class="delete">
+                </div>`;
+    return deleteButton;
+};
+
+function renderForm(){
+    let form = `<form>
+                <input type="text" name="name" class="name"
+         placeholder="NAME"><br>
+                <input type="text" name="price" class="price"
+        placeholder="PRICE">
+                <br>
+                <input type="text" name="link" class="link"
+        placeholder="LINK">
+                </form>`
+    return form;
+};
+
+function renderStars(){
+    let stars = `<div class="stars"></div>`;
+    return stars;
+};
+
 //
 //
 //function starHoverResponse(){

a5e030a1118d3a44673f21242c739a1a206dddd5
A'Jahn Kimble <ajahnkimble@gmail.com>
2019-11-09 22:19:18 -0500

    added renderlanding handleLandingStart and scrollWindow functions to renderToPage

--- Twitter/Get Tweet/renderToPage.js
+++ Twitter/Get Tweet/renderToPage.js
@@ -1,8 +1,32 @@
+//get reply from api-call
+//render to page
 function renderTweet(reply) {
-   
-    let tweet = reply.statuses[0].text;
+  let tweet = reply.statuses[0].text;
     $("body").append(
      `<p>${tweet}</p>`
     )
+};
+
+function renderLanding() {
+    return(
+    `<section class="js-landing-page landing-page">
+        
+           <p class="js-short-story short-story">
+           ${shortStory}</p>
+
+           <p class="js-explantion explanation">
+           ${explantion}</p>
+
+           <button class="js-start start">Start<button
+
+    </section>`)
+}
+
+function handleLandingStart() {
+    $(".js-landing-page").on("submit",(e) => {
+        e.preventDefault();
+        scrollWindow();
+        STORE.scroll += window.innerHeight; 
+    })
 }
 
